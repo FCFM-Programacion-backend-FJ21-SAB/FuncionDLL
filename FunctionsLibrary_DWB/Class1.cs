eeefedc9fdb44ef1b71cbe245a0b13c43fd318f3
Rolando <rolando.fcfm2020@gmail.com>
2021-02-20 16:37:49 -0600

    first commit

---
+++ FunctionsLibrary_DWB/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FunctionsLibrary_DWB
+{
+    public class Function
+    {
+
+        public decimal Sum(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public decimal Mult(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+
+    }
+}
